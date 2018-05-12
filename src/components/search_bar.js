d24fa50d607fc66ff9ece9ff2218a56c2044c6ce
Robert Zimmerman <zim2007@gmail.com>
2018-05-11 21:48:04 -0700

    init commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            term: 1
+        };
+    }
+   
+
+    render(){
+        return(
+            <div>
+                <input 
+                value={this.state.term} 
+                onChange={this.onInputChange}/>
+                Value of the input: {this.state.term}
+            </div>
+
+        );
+    }
+    onInputChange = (event) => {
+        this.setState({term: this.state.term * 2});
+        console.log(this.state.term);
+    }
+    
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyAD6mJp0fz8vUPx1beF7tYsHK6e4d3g-Ig';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+    constructor(props){
+        super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+        this.state =  {
+            videos: [],
+            selectedVideo: null,
+        };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        YTSearch({key: API_KEY, term: 'Pearl Jam'}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0],
+            });
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+            );
+        }
+}
+//Take this component's generated html 
+//and put it on the page (DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
